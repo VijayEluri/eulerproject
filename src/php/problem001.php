bc92f350bd4139953b2b8aac402f79c6e4955582
Pascal <pascal@pascaldimassimo.com>
2010-12-09 16:19:32 -0500

    Added Problem 001 in PHP

---
+++ src/php/problem001.php
@@ -0,0 +1,16 @@
+<?php
+
+	function problem001($upper) {
+		$total = 0;
+		for ($i = 3; $i < $upper; $i++) {
+			if ($i % 3 == 0 or $i % 5 == 0) {
+				$total += $i;
+			}
+		}
+		return $total;		
+	}
+
+	$res = problem001(1000);
+
+	echo "$res\n";
+?>
